8ac68368939f7fa676949aaee575ebf67bc35544
Matheus <maatsimeao@gmail.com>
2023-03-01 12:20:55 -0300

    atualizado

--- projeto/script.js
+++ projeto/script.js
@@ -1,11 +1,11 @@
 import initAnimais from "./modulos/init-animais.js"
 import initFaq from "./modulos/init-faq.js"
 import initScroll from "./modulos/init-scroll.js"
+import {initModal, fecharModal} from "./modulos/modal.js"
+
 
 initAnimais()
 initFaq()
 initScroll()
-
-/* TREINANDO */
-
-/* TREINANDO */
+initModal()
+fecharModal()
